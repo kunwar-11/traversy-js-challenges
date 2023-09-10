9fff473c3b5abaccc1386a057f5d4284a49c6d68
Rishabh Kunwar <kunwarrishabh70@gmail.com>
2023-09-10 23:35:37 +0530

    challenge 3

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,19 @@
-function calculator() {}
+function calculator(num1, num2, op) {
+  let result;
+
+  if (op === "+") {
+    result = num1 + num2;
+  } else if (op === "-") {
+    result = num1 - num2;
+  } else if (op === "*") {
+    result = num1 * num2;
+  } else if (op === "/") {
+    result = num1 / num2;
+  } else {
+    throw new Error("Invalid operator");
+  }
+
+  return result;
+}
 
 module.exports = calculator;
