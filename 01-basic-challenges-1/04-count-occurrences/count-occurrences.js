a22a71d34278d8b2864ed833c47988e43b81274a
Rishabh Kunwar <kunwarrishabh70@gmail.com>
2023-09-11 13:44:34 +0530

    challenege 4 count occurence

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,12 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let occurence = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      occurence++;
+    }
+  }
+
+  return occurence;
+}
 
 module.exports = countOccurrences;
