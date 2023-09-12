0455a487d9f70d83ae792185dec9bb6a422a8717
Rishabh Kunwar <kunwarrishabh70@gmail.com>
2023-09-12 08:25:36 +0530

    find max number chal-5

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,12 @@
-function findMaxNumber() {}
+function findMaxNumber(numberArr) {
+  let max = 0;
+  for (let i = 0; i < numberArr.length; i++) {
+    if (numberArr[i] > max) {
+      max = numberArr[i];
+    }
+  }
+
+  return max;
+}
 
 module.exports = findMaxNumber;
