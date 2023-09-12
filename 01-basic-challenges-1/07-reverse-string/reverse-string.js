8d423afd6bbc9dd8f87d3c7cabbf9e425ae9451d
Rishabh Kunwar <kunwarrishabh70@gmail.com>
2023-09-12 19:00:59 +0530

    palindrome challenge

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,9 @@
-function reverseString() {}
+function reverseString(str) {
+  let reverseStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseStr += str[i];
+  }
+  return reverseStr;
+}
 
 module.exports = reverseString;
--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,41 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  const formatted = removeAlphaNumeric(str.toLowerCase());
+  const reversed = reversedString(formatted);
+  if (formatted === reversed) {
+    return true;
+  }
+  return false;
+}
+
+function removeAlphaNumeric(str) {
+  let formatted = "";
+  for (let i = 0; i < str.length; i++) {
+    if (isAlphaNumeric(str[i])) {
+      //removed spl char and spaces
+      formatted += str[i];
+    }
+  }
+
+  return formatted;
+}
+
+function isAlphaNumeric(char) {
+  const charCode = char.charCodeAt(0);
+
+  if (
+    (charCode >= 48 && charCode <= 57) ||
+    (charCode >= 97 && charCode <= 122)
+  ) {
+    return true;
+  }
+  return false;
+}
+function reversedString(str) {
+  let rev = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    rev += str[i];
+  }
+  return rev;
+}
 
 module.exports = isPalindrome;
