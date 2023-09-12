71bc176ad950dc247e8c7bae49db4196379a00fa
Rishabh Kunwar <kunwarrishabh70@gmail.com>
2023-09-12 13:44:52 +0530

    title case challenge

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,10 @@
-function titleCase() {}
+function titleCase(str) {
+  const strArray = str.toLowerCase().split(" ");
+  for (let i = 0; i < strArray.length; i++) {
+    strArray[i] = strArray[i][0].toUpperCase() + strArray[i].slice(1);
+  }
+
+  return strArray.join(" ");
+}
 
 module.exports = titleCase;
