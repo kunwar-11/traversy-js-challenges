3549ffbde7ec15ef39b77bc4bc708f6c641870a5
Rishabh Kunwar <kunwarrishabh70@gmail.com>
2023-09-12 19:55:16 +0530

    count vowel challenge

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,19 @@
-function countVowels() {}
+function countVowels(str) {
+  let vowelCount = 0;
+  const lowerStr = str.toLowerCase();
+  for (let i = 0; i < lowerStr.length; i++) {
+    if (
+      lowerStr[i] === "a" ||
+      lowerStr[i] === "o" ||
+      lowerStr[i] === "e" ||
+      lowerStr[i] === "i" ||
+      lowerStr[i] === "u"
+    ) {
+      vowelCount++;
+    }
+  }
+
+  return vowelCount;
+}
 
 module.exports = countVowels;
